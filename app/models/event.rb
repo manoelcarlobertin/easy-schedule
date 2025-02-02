671b2892afcc4150869b07c6e5e789987017c3e5
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-02-02 15:37:41 -0300

    ajuste nos testes da data de início

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,8 @@ class Event < ApplicationRecord
   # o campo pode name allow_blank (vazio ou nil) sem que a validação seja aplicada aqui.
   validates :name, presence: true, length: { minimun: 2, maximum: 200, allow_blank: true }
   validates :description, presence: true
-  validates :started_at, presence: true
+  validates :started_at, presence: true, allow_blank: true
   validates :finished_at, presence: true
-  # validate :validate_if_starts_in_future, on: :create
   validate :finished_after_started
   # repara que é no singular (validate) pra chamar o método;
 
@@ -13,17 +12,10 @@ class Event < ApplicationRecord
   # confere se os campos estão preenchidos e compara as datas
   def finished_after_started
     if finished_at.present? && started_at.present? && finished_at < started_at
-    
-    # debugger
-    
+
+      # debugger
+
       errors.add(:finished_at, "deve ser depois da data de início")
     end
   end
-
-  # confere se a data de início está no futuro (validate) pra chamar o método;
-  # def validate_if_starts_in_future
-  #   if started_at.present? && started_at < Time.now
-  #     errors.add(:started_at, "deve ser no futuro")
-  #   end
-  # end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -24,12 +24,6 @@ RSpec.describe Event, type: :model do
       expect(event.errors[:description]).to include("can't be blank")
     end
 
-    it 'is invalid without a started_at' do
-      event = Event.new(name: 'Evento', description: 'Descrição', finished_at: Time.current + 1.hour)
-      expect(event).not_to be_valid
-      expect(event.errors[:started_at]).to include("can't be blank")
-    end
-
     it 'is invalid without a finished_at' do
       event = Event.new(name: 'Evento', description: 'Descrição', started_at: Time.current)
       expect(event).not_to be_valid
@@ -47,27 +41,41 @@ RSpec.describe Event, type: :model do
       expect(event.errors[:finished_at]).to include('deve ser depois da data de início')
     end
 
-    # Verifica se started_at no passado é inválido, no futuro é válido e nulo é válido
+    # Verifica se started_at no passado é válido, no futuro é válido e nulo é válido
     context 'when started_at is in the past' do
-      it 'is not valid' do
-        event = Event.new(started_at: 1.hour.ago)
-
-        expect(event).not_to be_valid
-        expect(event.errors[:started_at]).to include("deve ser no futuro")
-      end
-    end
-
-    context 'when started_at is in the future' do
       it 'is valid' do
-        event = Event.new(started_at: 1.hour.from_now)
+        event = Event.new(
+          name: 'Evento válido',
+          description: 'iniciou no passado',
+          started_at: 1.hour.ago,
+          finished_at: Time.current
+        )
 
         expect(event).to be_valid
       end
     end
 
+    context 'when started_at is in the future' do
+      it 'is NOT valid' do
+        event = Event.new(
+          name: 'Evento inválido',
+          description: 'Sem data de início',
+          started_at: Time.current + 1.hour,
+          finished_at: Time.current - 1.hour
+        )
+
+        expect(event).not_to be_valid
+      end
+    end
+
     context 'when started_at is not present' do
       it 'is valid' do
-        event = Event.new(started_at: nil)
+        event = Event.new(
+          name: 'Evento válido',
+          description: 'Sem data de início',
+          started_at: nil,
+          finished_at: Time.current - 1.hour
+        )
 
         expect(event).to be_valid
       end
